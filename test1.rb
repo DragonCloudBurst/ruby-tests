fab077400d0df0a3841c82ca4638890719b562a1
Katie Spivey <katieaspivey@gmail.com>
2025-04-15 12:14:54 -0400

    directory tests

--- test1.rb
+++ test1.rb
@@ -8,3 +8,16 @@ puts y
 
 z = 1 + 3
 puts z
+
+# returns false
+puts(1 == 2)
+# returns true
+puts(1 ==1)
+
+# working with files
+# pwd and mkdir just like in shell commands
+Dir.chdir("C:\\")
+# Dir.mkdir("C:\\ruby_test_1")
+test_1 = Dir.chdir("C:\\ruby_test_1")
+directory = Dir.pwd
+puts directory
